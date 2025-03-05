3c2aaeeba791249c82e4bb5ee96cdb2887dfee0d
EchoRan6319 <EchoRan6319@outlook.com>
2025-03-05 19:13:36 +0800

    Update

---
+++ Project20250305/Project20250305/源.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*作业1
+int main() {
+    int x, y, z;
+    float n;
+
+    printf("请输入x, y, z的值：（使用空格隔开）");
+    scanf_s("%d %d %d", &x, &y, &z);
+
+    n = x * x + (y * z) / 2.0; 
+    printf("n = %.2f\n", n);
+
+    return 0;
+}
+*/
+
+/*作业2
+int main() {
+    int x = 97, y = 98, z = 99;
+    printf("x=%d\ny=%d\nz=%d\n", x, y, z);
+    printf("x=%c\ty=%c\tz=%d\n", x, y, z);
+}
+*/
+
+double main() {
+    double a = 3.6;
+	double v = 3.6 * 3.6 * 3.6;
+	printf("v = %.2f\n", v);
+    return 0;
+}
